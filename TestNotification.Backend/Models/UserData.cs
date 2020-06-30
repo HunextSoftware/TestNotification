55e255890f43b1256abed89692d4c2c978c854dc
AlbertoGobbo <47864124+AlbertoGobbo@users.noreply.github.com>
2020-06-30 09:35:55 +0200

    add persistence layer

---
+++ TestNotification.Backend/Models/UserData.cs
@@ -0,0 +1,16 @@
+﻿using LiteDB;
+using System;
+
+namespace TestNotificationBackend.Models.Database
+{
+    public class UserData
+    {
+        [BsonId]
+        public Guid GUID { get; set; }
+        public string Url { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Company { get; set; }
+        public string SectorCompany { get; set; }
+    }
+}
