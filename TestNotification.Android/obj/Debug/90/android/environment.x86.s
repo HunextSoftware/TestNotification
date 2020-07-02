e95b3c4421b1f82086e94815c3a96a6ae017729c
AlbertoGobbo <47864124+AlbertoGobbo@users.noreply.github.com>
2020-07-02 12:02:51 +0200

    update keyboard functionality

--- TestNotification.Android/obj/Debug/90/android/environment.x86.s
+++ TestNotification.Android/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"d2ba4013-edb7-4f9d-9ada-d0a86d9e765c"
+	.asciz	"6f357bbe-884a-4b58-8d54-6dfdd7aeb3ea"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
