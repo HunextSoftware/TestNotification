9253c1db8723839d1729e26e82b482a6c4069d44
AlbertoGobbo <47864124+AlbertoGobbo@users.noreply.github.com>
2020-07-31 14:28:39 +0200

    improve a better badge counter
    Not work like it would have to...signal it with a new issue for future improvements

--- TestNotificationWebApp/Program.cs
+++ TestNotificationWebApp/Program.cs
@@ -15,7 +15,6 @@ namespace TestNotificationWebApp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    //webBuilder.UseUrls("https://localhost:6001;http://localhost:6000");
                 });
     }
 }
