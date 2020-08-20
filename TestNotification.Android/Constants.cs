4cf89e815042ec7ee65fc30624fb7bfb2e095c3a
AlbertoGobbo <gobboalberto98@gmail.com>
2020-08-20 10:04:44 +0200

    Delete all project secrets which are remained public until now

--- TestNotification.Android/Constants.cs
+++ TestNotification.Android/Constants.cs
@@ -2,10 +2,6 @@
 {
     public static class Constants
     {
-        // Azure's constants
-        public const string NotificationHubName = "testpushnotificationhub";
-        public const string ListenConnectionString = "Endpoint=sb://testpushnotificationhubns.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=JAqNrRS/eArWTIROSMjqYwzSlMWRuL9SB1E1Evyx3LQ=";
-        
         // Notification channel's constants
         public const int NOTIFICATION_ID = 1000;
         public const string CHANNEL_ID = "TestNotificationChannel";
